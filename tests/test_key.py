446479bf0d434edcf7ab2a2cc53e819f640e6710
dmachard <d.machard@gmail.com>
2020-09-27 16:38:28 +0200

    new testunit for key generation

--- tests/test_key.py
+++ tests/test_key.py
@@ -1,5 +1,8 @@
-
 from dnsdist_console import Key
+import unittest
 
-k = Key().generate()
-print(k)
+class TestConnect(unittest.TestCase):
+    def test_generate_key(self):
+        """generate key in base64"""             
+        k = Key().generate()
+        self.assertRegex(k, "[a-zA-Z0-9+\/]+={0,2}")
